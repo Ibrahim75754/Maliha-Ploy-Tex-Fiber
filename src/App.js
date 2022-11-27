6b5b091bcbc06ea0337ec40cbd41474622de40b9
Md. Ibrahim Hossain <ih75754@gmail.com>
2022-11-28 00:03:28 +0600

    done

--- src/App.js
+++ src/App.js
@@ -7,18 +7,22 @@ function App() {
     setColor(e.target.value)
   }
   return (
-    <div className="vh-100 d-flex justify-content-center align-items-center bg-secondary">
-      <div className='w-25'>
-        <h1 data-aos="fade-down"
-          data-aos-duration="3000">Maliha Poly Tex Fiber Industry Limited.</h1>
-        <p className='text-warning' data-aos="fade-right"
-          data-aos-duration="3000" >Technical Task</p>
-        <textarea class="form-control" style={{ backgroundColor: `${color}` }} name="" id="" cols="30" rows="10" data-aos="fade-left"
-          data-aos-duration="3000"></textarea><br />
+    <div className="bg-info">
+      <div className="container">
+        <div className="row vh-100 d-flex justify-content-center align-items-center ">
+          <div className='col-md-3'>
+            <h1 data-aos="fade-down"
+              data-aos-duration="3000">Maliha Poly Tex Fiber Industry Limited.</h1>
+            <p data-aos="fade-right"
+              data-aos-duration="3000" >Technical Task</p>
+            <textarea class="form-control" style={{ backgroundColor: `${color}` }} name="" id="" cols="30" rows="10" data-aos="fade-left"
+              data-aos-duration="3000"></textarea><br />
 
-        <input class="form-control" onChange={(e) => changeColor(e)} type="text" placeholder='Input Color name or code' data-aos="fade-up"
-          data-aos-duration="3000" />
+            <input class="form-control" onChange={(e) => changeColor(e)} type="text" placeholder='Input Color name or code' data-aos="fade-up"
+              data-aos-duration="3000" />
 
+          </div>
+        </div>
       </div>
     </div>
   );
