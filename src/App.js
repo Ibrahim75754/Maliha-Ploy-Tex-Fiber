1db8fa301d4473866e6474727b1d359c9158e98f
Md. Ibrahim Hossain <ih75754@gmail.com>
2022-11-27 23:40:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [color, setColor] = useState("");
+  const changeColor = (e) => {
+    setColor(e.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="vh-100 d-flex justify-content-center align-items-center bg-secondary">
+      <div className='w-25'>
+        <h1 data-aos="fade-down"
+          data-aos-duration="3000">Maliha Poly Tex Fiber Industry Limited.</h1>
+        <p className='text-warning' data-aos="fade-right"
+          data-aos-duration="3000" >Technical Task</p>
+        <textarea class="form-control" style={{ backgroundColor: `${color}` }} name="" id="" cols="30" rows="10" data-aos="fade-left"
+          data-aos-duration="3000"></textarea><br />
+
+        <input class="form-control" onChange={(e) => changeColor(e)} type="text" placeholder='Input Color name or code' data-aos="fade-up"
+          data-aos-duration="3000" />
+
+      </div>
     </div>
   );
 }
